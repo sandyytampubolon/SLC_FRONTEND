95b3c0474377490dccd28f3aae0d7b9401c189b3
Vincent GG <centgint@gmail.com>
2025-03-31 01:01:39 +0700

    Perbaikan Schedule meet past time

--- mysite/chat/forms.py
+++ mysite/chat/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Meeting
+from django.utils import timezone
 
 class MeetingForm(forms.ModelForm):
     schedule_meeting = forms.BooleanField(required=False, label='Schedule for later')
@@ -10,3 +11,10 @@ class MeetingForm(forms.ModelForm):
         widgets = {
             'start_time': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
         }
+
+    #menangani error jika start_time masa lalu
+    def clean_start_time(self):
+        start_time = self.cleaned_data.get('start_time')
+        if start_time and start_time < timezone.now():
+            raise forms.ValidationError("Start time cannot be in the past")
+        return start_time
